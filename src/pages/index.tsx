7d4d3b140203376d61eaa0873e7f8cbb961fef30
Oscar Newman <newman.oscar@gmail.com>
2020-12-09 11:04:58 -0600

    Cleanup index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ type Props = {}
 
 function Home(props: Props) {
 	return (
-		<div className="prose">
+		<div className="container mx-auto prose">
 			<h1>Hey there</h1>
 			<p className="lead">This is some text</p>
 		</div>
